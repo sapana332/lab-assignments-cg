5e859dd87ea7d63ccf39264d43578841bd38b14a
sapana332 <76993419+sapana332@users.noreply.github.com>
2021-01-12 23:29:59 +0530

    Add files via upload

---
+++ inheritance and polymorphism/lab4ques3/main/Main.java
@@ -0,0 +1,17 @@
+package items.domain.main;
+
+
+
+	import items.domain.util.Util;
+
+	public class Main {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	Util util=new Util();
+	util.util();
+		}
+
+	}
+
+
